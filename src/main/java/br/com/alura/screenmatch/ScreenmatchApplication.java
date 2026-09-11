01629ddb402266521d582c09267675c07c692693
Weslley Gatti <weslleygatti1@hotmail.com>
2026-09-11 14:55:46 -0300

    Adicionado o consumo de API's

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,8 +1,11 @@
 package br.com.alura.screenmatch;
 
+import java.util.function.Consumer;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import br.com.alura.screenmatch.service.ConsumoAPI;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -12,6 +15,11 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	}
 
 	public void run(String... args) throws Exception{
-		System.out.println("Primeiro projeto Spring sem web");
+		var consumoAPI = new ConsumoAPI(); 
+		String api = "https://www.omdbapi.com/?t=gilmore+girls&Season=1&apikey=4ef38559";
+		var json = consumoAPI.obterDados(api);
+		System.out.println(json);
+		json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
 	}
 }
